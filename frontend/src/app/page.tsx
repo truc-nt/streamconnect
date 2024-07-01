29706d8785a2277b3a1f4af27e05fe087e76cf1f
catkhanhnguyen123 <hrin2211@gmail.com>
2024-07-01 16:29:54 +0700

    fix layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,15 +7,23 @@ import SuggestLivestreams from '@/components/SuggestLivestreams';
 
 export default function Home() {
   return (
-    <Box sx={{ display: 'flex', height: '100vh' }}>
+    <Box sx={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
       <Box sx={{ width: 272, flexShrink: 0 }}>
         <NavigationBoard />
       </Box>
       <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-        <Box sx={{ position: 'relative', width: '100%' }}>
-          <Header />
-        </Box>
-        <Box sx={{ flexGrow: 1, padding: 2, backgroundColor: '#1c1c1c', overflowY: 'auto' }}>
+        <Header />
+        <Box
+          sx={{
+            flexGrow: 1,
+            padding: 2,
+            backgroundColor: '#1c1c1c',
+            overflowY: 'auto',
+            '::-webkit-scrollbar': { display: 'none' },
+            '-ms-overflow-style': 'none',
+            'scrollbar-width': 'none',
+          }}
+        >
           <Box sx={{ marginX: 1.5 }}>
             <CategorySlider />
             <LivestreamPreview />
