f5b4251d8503a775c090752d8f7487b6ba7d1e38
vutranhoang <hoang@palexy.com>
2024-09-08 17:54:44 +0700

    remove unused code

--- api_java/src/main/java/com/hcmut/streamconnect/model/service/AuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.hcmut.streamconnect.model.service;
-
-import com.hcmut.streamconnect.controller.AuthenticationController.LoginUserDto;
-import com.hcmut.streamconnect.model.entity.Account;
-
-public interface AuthenticationService {
-    Account authenticate(LoginUserDto input);
-    Account register(Account account);
-}
